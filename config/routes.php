331997e08b96f2ef2aeccef5dedfd08606dd43bd
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-08-07 13:09:37 +0530

    Verify Authentication Is Enabled

--- config/routes.php
+++ config/routes.php
@@ -47,6 +47,9 @@ use Cake\Routing\Route\DashedRoute;
 Router::prefix('api', function ($routes) {
     $routes->extensions(['json', 'xml']);
     $routes->resources('Cocktails');
+    $routes->resources('Users');
+    Router::connect('/api/users/register', ['controller' => 'Users', 'action' => 'add', 'prefix' => 'api']);
+    $routes->fallbacks('InflectedRoute');
 });
 
 Router::defaultRouteClass(DashedRoute::class);
